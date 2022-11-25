eccab4349c0841e1e987689ff9245590e9e5ccad
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-25 02:28:05 +0000

    Java基礎編4章 確認問題 問題

---
+++ Chapter04.java
@@ -0,0 +1,14 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+        String str1;
+        str1 = "Yuya";
+        System.out.println(str1);
+        
+        str1 = "Java";
+        System.out.println(str1);
+        
+        final int NUMBER;
+        NUMBER = 7;
+        System.out.println(NUMBER);
+    }
+}
