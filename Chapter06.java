f4f45f0e606114e43e8b0feb2649fc31887818e3
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-26 09:50:33 +0000

    Java基礎編6章 文字列の連結

--- Chapter06.java
+++ Chapter06.java
@@ -1,20 +1,11 @@
 public class Chapter06 {
     public static void main(String[] args) {
-        boolean a = true;
-        boolean b = false;
-        boolean c;
-        
-        c = a && true;   // c = true
+        String a = "100";
+        String b = "200";
+        String c;
+        c = a + b;        // c = 100200
         System.out.println(c);
-        c = a && false;  // c = false
-        System.out.println(c);
-        c = b || true;   // c = true
-        System.out.println(c);
-        c = b || false;  // c = false
-        System.out.println(c);
-        c = !a;          // c = false
-        System.out.println(c);
-        c = !b;          // c = true
+        c = "あ" + "い";   // c = あい
         System.out.println(c);
 
     }
