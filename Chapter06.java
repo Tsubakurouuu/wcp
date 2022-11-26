53ea71c2a6b96cbb72cb051b937f9fef633cbc35
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-26 10:04:11 +0000

    Java基礎編6章 確認問題 問題

--- Chapter06.java
+++ Chapter06.java
@@ -1,26 +1,19 @@
 public class Chapter06 {
     public static void main(String[] args) {
-        String a = "applePie";
-        String b = "applePie";
-        boolean c;
-        c = a == b;  // c = true
-        System.out.println(c);
+        int val = 2 + 5;
+        System.out.println(val);
 
-        String d = "apple";
-        String e = d + "Pie";
-        c = a == e;  // c = false
-        System.out.println(c);
+        val++;
+        System.out.println(val);
 
-        String f = new String("applePie");
-        c = a == f;  // c = false
-        System.out.println(c);
+        val *= 5;
+        System.out.println(val);
 
-        // 変数の中身はすぐ上の例と同じ
-        c = a.equals(b);  // c = true
-        System.out.println(c);
-        c = a.equals(e);  // c = true
-        System.out.println(c);
-        c = a.equals(f);  // c = true
-        System.out.println(c);
+        boolean bool;
+        bool = val == 50;
+        System.out.println(bool);
+
+        bool = val < 50 && val % 10 == 0;
+        System.out.println(bool);
     }
 }
