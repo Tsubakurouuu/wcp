0dbe0b6139e8dbc50dcf2591e743b1ba2b78078e
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-26 09:57:52 +0000

    Java基礎編6章 文字列の等価比較

--- Chapter06.java
+++ Chapter06.java
@@ -5,5 +5,22 @@ public class Chapter06 {
         boolean c;
         c = a == b;  // c = true
         System.out.println(c);
+
+        String d = "apple";
+        String e = d + "Pie";
+        c = a == e;  // c = false
+        System.out.println(c);
+
+        String f = new String("applePie");
+        c = a == f;  // c = false
+        System.out.println(c);
+
+        // 変数の中身はすぐ上の例と同じ
+        c = a.equals(b);  // c = true
+        System.out.println(c);
+        c = a.equals(e);  // c = true
+        System.out.println(c);
+        c = a.equals(f);  // c = true
+        System.out.println(c);
     }
 }
