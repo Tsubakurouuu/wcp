d77f666f75dcfc82243d6ba9db57d3231e776ae0
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-26 09:29:07 +0000

    Java基礎編6章 算術演算子

---
+++ Chapter06.java
@@ -0,0 +1,16 @@
+public class Chapter06 {
+    public static void main(String[] args) {
+        int a = 15;
+        int c;
+        c = a + 3;  // c = 18
+        System.out.println(c);
+        c = a - 5;  // c = 10
+        System.out.println(c);
+        c = a * 2;  // c = 30
+        System.out.println(c);
+        c = a / 6;  // c = 2
+        System.out.println(c);
+        c = a % 4;  // c = 3
+        System.out.println(c);
+    }
+}
