89ee2068751be4dcf00af92dc71b441135042d18
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-26 09:37:41 +0000

    Java基礎編6章 インクリメント・デクリメント演算子 前置と後置

--- Chapter06.java
+++ Chapter06.java
@@ -1,11 +1,17 @@
 public class Chapter06 {
     public static void main(String[] args) {
         int a;
-        a = 10;
-        a++;
-        System.out.println(a);
-        a = 10;
-        a--;
-        System.out.println(a);
+        int c;
+        
+        a = 5;
+        c = ++a;  // a をインクリメントした後に、a が c に代入される
+        System.out.println(c);
+        // それぞれの値は、a が「6」で c も「6」
+        
+        a = 5;
+        c = a++;  // a を c に代入した後に、a がインクリメントされる
+        System.out.println(c);
+        // それぞれの値は、a が「6」で c が「5」
+        
     }
 }
