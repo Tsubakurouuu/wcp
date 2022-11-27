874ca312d221edf3b862660a131e55d0dc1a1b86
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-27 08:42:39 +0000

    Java基礎編7章 確認問題 問題1

--- Chapter07.java
+++ Chapter07.java
@@ -1,15 +1,14 @@
 public class Chapter07 {
     public static void main(String[] args) {
-        int val = 2;
-        switch (val) {
-        case 3:
-            System.out.println("よくできました！");
-            break;
-        case 2:
-            System.out.println("ふつうでした！");
-            break;
-        default:
-            System.out.println("がんばろう！");
+        int distance = 11; // 
+        if (distance <= 5) {
+            System.out.println("とても近いです");
+        } else if (distance <= 10) {
+            System.out.println("近いです");
+         } else if (distance <= 15) {
+            System.out.println("遠いです");
+        } else {
+            System.out.println("とても遠いです");
         }
     }
 }
