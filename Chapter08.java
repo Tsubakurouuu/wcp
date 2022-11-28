6e2ecca951a4755f7efe435bdea1b649f69264e2
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-28 02:28:50 +0000

    Java基礎編8章 break文、continue文 break文

--- Chapter08.java
+++ Chapter08.java
@@ -1,11 +1,11 @@
 public class Chapter08 {
   public static void main(String[] args) {
-    int[] array = { 1, 2, 3, 4, 5 };
-    // 拡張for文：
-    // 配列の要素を順番にnumberに代入し、要素分繰り返す
-    for (int number : array) {
-      // ブロック：numberを出力
-      System.out.println("Chapter08 = " + number);
+    for (int count = 0; count < 5; count++) {
+      if (count == 2) {
+        // countが2の場合、for文を終了
+        break;
+      }
+      System.out.println("Chapter08 = " + count);
     }
   }
 }
