ce32af9b15554c2c2f127129d4e46a8051053349
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-28 02:00:15 +0000

    Java基礎編8章 while文の繰り返し サンプルプログラムの説明

--- Chapter08.java
+++ Chapter08.java
@@ -1,10 +1,13 @@
 public class Chapter08 {
     public static void main(String[] args) {
-        String color = "blue"; 
         int number = 1;
+        // while文：ループ開始
+        // numberが50未満の場合、ブロックの処理を行なう
         while (number < 50) {
-          number *= 2;
-          System.out.println("Chapter08 = " + number);
+            // ブロック：
+            // numberにnumber * 2 を代入して、numberを出力
+            number *= 2;
+            System.out.println("While01 = " + number);
         }
     }
 }
