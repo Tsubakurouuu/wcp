9b7eeab9cad8533aa015674c078fcafa864d9d15
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-28 02:16:13 +0000

    Java基礎編8章 for文での繰り返し for文

--- Chapter08.java
+++ Chapter08.java
@@ -1,14 +1,7 @@
 public class Chapter08 {
   public static void main(String[] args) {
-    int number = 1;
-    // do-while文：ループ開始
-    do {
-        // ブロック：
-        // numberにnumber * 2 を代入して、numberを出力
-        number *= 2;
-        System.out.println("Chapter08 = " + number);
-    // do-while文：
-    // numberが50未満の場合、ブロックの処理をループ実行する
-    } while (number < 50);
+    for (int number = 1; number <= 5; number ++) {
+      System.out.println("Chapter08 = " + number);
+    }
   }
 }
