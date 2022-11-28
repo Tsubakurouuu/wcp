9f4b45a30f60b22fa4b4e14658b8e547786f40bc
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-28 02:04:39 +0000

    Java基礎編8章 while文の繰り返し do-while文

--- Chapter08.java
+++ Chapter08.java
@@ -1,13 +1,9 @@
 public class Chapter08 {
-    public static void main(String[] args) {
-        int number = 1;
-        // while文：ループ開始
-        // numberが50未満の場合、ブロックの処理を行なう
-        while (number < 50) {
-            // ブロック：
-            // numberにnumber * 2 を代入して、numberを出力
-            number *= 2;
-            System.out.println("While01 = " + number);
-        }
-    }
+  public static void main(String[] args) {
+    int number = 1;
+    do {
+      number *= 2;
+      System.out.println("While01 = " + number);
+    } while (number < 50); 
+  }
 }
