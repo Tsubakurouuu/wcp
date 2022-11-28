68a713ee66df6dbd9b3a45cbcf8b8fe7038a6a55
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-28 02:25:04 +0000

    Java基礎編8章 for文での繰り返し 拡張for分 サンプルプログラムの説明

--- Chapter08.java
+++ Chapter08.java
@@ -1,8 +1,11 @@
 public class Chapter08 {
   public static void main(String[] args) {
     int[] array = { 1, 2, 3, 4, 5 };
+    // 拡張for文：
+    // 配列の要素を順番にnumberに代入し、要素分繰り返す
     for (int number : array) {
-      System.out.println("Chapter08 = " + number )
+      // ブロック：numberを出力
+      System.out.println("Chapter08 = " + number);
     }
   }
 }
