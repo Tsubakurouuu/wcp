c11a03e926017525ee1b1103c6b976c4639acb10
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-28 02:47:48 +0000

    Java基礎編8章 break文、continue文 再帰処理

--- Chapter08.java
+++ Chapter08.java
@@ -1,11 +1,16 @@
 public class Chapter08 {
   public static void main(String[] args) {
-    for (int count = 0; count < 5; count++) {
-      if (count == 2) {
-        // countが2の場合、この後の処理をスキップ
-        continue;
-      }
-      System.out.println("Chapter08 = " + count);
+    int number = 1;
+    printNum(number);
+  }
+
+  public static int printNum(int number) {
+    if (number < 50) {
+      number *= 2;
+      System.out.println("Chapter08 = " + number);
+      // 条件にマッチしたら自分自身を再度呼び出す
+      printNum(number);
     }
+    return number;
   }
 }
