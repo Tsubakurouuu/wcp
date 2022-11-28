433d2dcb74adc61966ccea5313fae0e490e3aaee
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-28 01:55:16 +0000

    Java基礎編8章 while文の繰り返し while文

---
+++ Chapter08.java
@@ -0,0 +1,10 @@
+public class Chapter08 {
+    public static void main(String[] args) {
+        String color = "blue"; 
+        int number = 1;
+        while (number < 50) {
+          number *= 2;
+          System.out.println("Chapter08 = " + number);
+        }
+    }
+}
