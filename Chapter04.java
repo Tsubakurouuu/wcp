dd55b453e5b38882704223414ae6aff906a3f787
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-03 01:00:45 +0000

    Java基礎編4章 定数 サンプルプログラムの説明

--- Chapter04.java
+++ Chapter04.java
@@ -1,16 +1,10 @@
 public class Chapter04 {
     public static void main(String[] args) {
-        int num1;  // int型の変数num1を宣言
-        String str1;  // String型の変数str1を宣言
-        num1 = 10;  // num1を初期化
-        str1 = "Hello World!";  //str1を初期化
-        System.out.println(num1);
-        System.out.println(str1);
+        // 定数の宣言
+        final int MIN_NUMBER = 10;
+        System.out.println(MIN_NUMBER);
 
-        num1 = 20;  // num1に20を再代入
-        System.out.println(num1);
-
-        int num2 = 30;  // int型の変数num2を宣言と同時に初期化
-        System.out.println(num2);
+        // 再代入しようとするとエラー
+        //MIN_NUMBER = 20;
     }
 }
