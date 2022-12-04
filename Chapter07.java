7bb6210710262db489d6a94070e0a9bb3162bc19
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-04 01:01:43 +0000

    Java基礎編7章 確認問題 問題1

--- Chapter07.java
+++ Chapter07.java
@@ -1,14 +1,14 @@
 public class Chapter07 {
     public static void main(String[] args) {
-        int number = 7;
-        if (number < 5) {
-            System.out.println(number + " は5未満です");
-        } else if (number < 10) {
-            System.out.println(number + " は5以上、10未満です");
-        } else if (number < 15) {
-            System.out.println(number + " は10以上、15未満です");
+        int d = 11;
+        if (d <= 5) {
+            System.out.println("とても近いです");
+        } else if (d <= 10) {
+            System.out.println("近いです");
+        } else if (d <= 15) {
+            System.out.println("遠いです");
         } else {
-            System.out.println(number + " は15未満ではないです");
+            System.out.println("とても遠いです");
         }
     }
 }
