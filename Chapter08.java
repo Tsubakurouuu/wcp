4ba917c379ab0febb174e8984293079be28c1a8b
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-05 01:48:58 +0000

    Java基礎編8章 break文、continue文 continue文

--- Chapter08.java
+++ Chapter08.java
@@ -2,7 +2,7 @@ public class Chapter08 {
     public static void main(String[] args) {
         for (int count = 0; count < 5; count++) {
             if (count == 2) {
-                break;
+                continue;
             }
             System.out.println("Chapter08 = " + count);
         }
