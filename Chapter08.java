0ae8f787798ab43a66c5ec5b5b1f5fd9f1d78614
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-05 02:12:06 +0000

    Java基礎編8章 確認問題 問題

--- Chapter08.java
+++ Chapter08.java
@@ -1,15 +1,21 @@
 public class Chapter08 {
     public static void main(String[] args) {
         int number = 1;
-        printNum(number);
-    }
-    
-    public static int printNum(int number) {
-        if (number < 50) {
-            number *= 2;
-            System.out.println("Chapter08 = " + number);
-            printNum(number);
+        while (number < 5) {
+            System.out.println(number * number);
+            number++;
         }
-        return number;
+
+        int[] array = { 1, 2, 3, 4 };
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+        for (int val : array) {
+            if (val % 2 == 0) {
+                continue;
+            }
+            System.out.println(val);
+        }
     }
 }
