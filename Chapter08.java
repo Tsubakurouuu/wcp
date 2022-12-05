c72c68b22be2d5a4716958d28f8b16032288aa9d
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-05 01:00:39 +0000

    Java基礎編8章 while文の繰り返し do-while文

--- Chapter08.java
+++ Chapter08.java
@@ -1,9 +1,9 @@
 public class Chapter08 {
     public static void main(String[] args) {
         int number =1;
-        while (number < 50) {
+        do {
             number *= 2;
             System.out.println("Chapter08 = " + number);
-        }
+        } while (number < 50);
     }
 }
