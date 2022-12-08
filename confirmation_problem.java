e92f5af7fcad831dcd73c69b0e203a7a7031a831
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-08 23:44:14 +0000

    Java基礎編3章 確認問題　　問題

--- confirmation_problem.java
+++ confirmation_problem.java
@@ -1,5 +1,6 @@
 public class confirmation_problem {
     public static void main(String[] args) {
-        System.out.println("こんにちは！");
+        System.out.println("Hello WEBCAMP!");
+        System.out.println("Hello Java!");
     }
 }
