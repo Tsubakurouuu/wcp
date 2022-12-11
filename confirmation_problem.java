1d8623b28b3bb06172881cad47dca05bcbfd9de9
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-11 23:08:15 +0000

    Java基礎編5章 確認問題　　問題

--- confirmation_problem.java
+++ confirmation_problem.java
@@ -1,14 +1,20 @@
 public class confirmation_problem {
     public static void main(String[] args) {
-        int val = 2 + 5;
-        System.out.println(val);
-        val++;
-        System.out.println(val);
-        val *=  5;
-        System.out.println(val);
-        boolean bool = val == 50;
-        System.out.println(bool);
-        bool = val < 50 && val % 10 == 0;
-        System.out.println(bool);
+      long l = 1234567890L;
+      System.out.println(l);
+      double d = 1.23;
+      System.out.println(d);
+      boolean b = true;
+      System.out.println(b);
+
+      Float f = 1.2345f;
+      System.out.println(f);
+      Integer i = 111;
+      System.out.println(i);
+      Character c = 'あ';
+      System.out.println(c);
+
+      int[] array = { 1, 2, 3, 4, 5 };
+      System.out.println(array[3]);
     }
 }
