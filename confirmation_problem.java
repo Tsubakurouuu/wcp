5d64aac3afaede7202bd7f5e2701f68cea1e2800
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-12 05:50:58 +0000

    R-3

--- confirmation_problem.java
+++ confirmation_problem.java
@@ -1,16 +1,15 @@
 public class confirmation_problem {
     public static void main(String[] args) {
       int i = 1;
-      int c = 0;
-      int s = 0;
+      String str = "";
       while (i <= 10) {
-        if (i % 2 == 0) {
-          c ++;
-          s += i;
+        if (i == 10) {
+          str = str + i;
+        } else {
+          str = str + i + ",";
         }
         i++;
       }
-      System.out.println("偶数は" + c +"個");
-      System.out.println("偶数の合計は" + s );
+      System.out.println(str);
     }
 }
