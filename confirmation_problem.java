1a4cf67ccce400e5292c8097522e6f6951fd9af5
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-12 06:38:56 +0000

    R-4

--- confirmation_problem.java
+++ confirmation_problem.java
@@ -1,15 +1,30 @@
 public class confirmation_problem {
     public static void main(String[] args) {
-      int i = 1;
-      String str = "";
-      while (i <= 10) {
-        if (i == 10) {
-          str = str + i;
-        } else {
-          str = str + i + ",";
-        }
-        i++;
+      int i = 5;
+      int b = 0;
+      int[] a = new int[20];
+      while (b < 20) {
+        a[b] = (b + 1) * 5;
+        b++;
       }
-      System.out.println(str);
+
+      b = 0;
+      String kisuu = "";
+      while (b <= 11) {
+        if (a[b] % 2 != 0) {
+          kisuu = kisuu + a[b] + ",";
+        }
+        b++;
+      }
+
+      String guusuu = "";
+      while (b < 20) {
+        if (a[b] % 2 == 0) {
+          guusuu = guusuu + a[b] + ",";
+        }
+        b++;
+      }
+      System.out.println(kisuu.substring(0, kisuu.length() - 1));
+      System.out.println(guusuu.substring(0, guusuu.length() - 1));
     }
 }
