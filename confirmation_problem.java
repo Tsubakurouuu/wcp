54184d5381d59ddd07b981243dcbc26cf9078cd8
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-12 04:50:25 +0000

    R-1

--- confirmation_problem.java
+++ confirmation_problem.java
@@ -1,20 +1,11 @@
 public class confirmation_problem {
     public static void main(String[] args) {
-      long l = 1234567890L;
-      System.out.println(l);
-      double d = 1.23;
-      System.out.println(d);
-      boolean b = true;
-      System.out.println(b);
-
-      Float f = 1.2345f;
-      System.out.println(f);
-      Integer i = 111;
-      System.out.println(i);
-      Character c = 'あ';
-      System.out.println(c);
-
-      int[] array = { 1, 2, 3, 4, 5 };
-      System.out.println(array[3]);
+      int i = 0;
+      while (i <= 10) {
+        if (i % 2 == 0) {
+          System.out.println(i);
+        }
+        i++;
+      }
     }
 }
