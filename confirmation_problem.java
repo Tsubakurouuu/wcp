6af13f4285c9755f7d44b12745f7385021d66771
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-13 06:32:40 +0000

    Java基礎編7章 確認問題　　問題1

--- confirmation_problem.java
+++ confirmation_problem.java
@@ -1,30 +1,14 @@
 public class confirmation_problem {
     public static void main(String[] args) {
-      int i = 5;
-      int b = 0;
-      int[] a = new int[20];
-      while (b < 20) {
-        a[b] = (b + 1) * 5;
-        b++;
+      int d = 9;
+      if (d < 5) {
+        System.out.println("とても近いです");
+      } else if (d < 10) {
+        System.out.println("近いです");
+      } else if (d < 15) {
+        System.out.println("遠いです");
+      } else {
+        System.out.println("とても遠いです");
       }
-
-      b = 0;
-      String kisuu = "";
-      while (b <= 11) {
-        if (a[b] % 2 != 0) {
-          kisuu = kisuu + a[b] + ",";
-        }
-        b++;
-      }
-
-      String guusuu = "";
-      while (b < 20) {
-        if (a[b] % 2 == 0) {
-          guusuu = guusuu + a[b] + ",";
-        }
-        b++;
-      }
-      System.out.println(kisuu.substring(0, kisuu.length() - 1));
-      System.out.println(guusuu.substring(0, guusuu.length() - 1));
     }
 }
