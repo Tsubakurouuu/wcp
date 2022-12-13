a19771cf8f27321b6e588cf4bcf1a8cea718cc11
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-13 06:42:01 +0000

    Java基礎編7章 確認問題　　問題2

--- confirmation_problem.java
+++ confirmation_problem.java
@@ -1,14 +1,18 @@
 public class confirmation_problem {
     public static void main(String[] args) {
-      int d = 9;
-      if (d < 5) {
-        System.out.println("とても近いです");
-      } else if (d < 10) {
-        System.out.println("近いです");
-      } else if (d < 15) {
-        System.out.println("遠いです");
-      } else {
-        System.out.println("とても遠いです");
-      }
+      String str = "yellow";
+      switch (str) {
+        case "red":
+          System.out.println("赤信号です");
+          break;
+        case "yellow":
+          System.out.println("黄信号です");
+          break;
+        case "blue":
+          System.out.println("青信号です");
+          break;
+        default:
+          System.out.println("信号の色ではありません");
+        }
     }
 }
