e9d576d63a99fcdb1667a08c1c1c699935b824b2
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-15 00:19:21 +0000

    スッキリわかるJava入門3.6 3-9

--- Main.java
+++ Main.java
@@ -1,15 +1,11 @@
 public class Main {
   public static void main(String[] args) {
-	  System.out.println("ようこそ占いの館へ");
-	  System.out.println("あなたの名前を入力してください");
-	  String name = new java.util.Scanner(System.in).nextLine();
-	  System.out.println("あなたの年齢を入力してください");
-	  String ageString = new java.util.Scanner(System.in).nextLine();
-	  int age = Integer.parseInt(ageString);
-	  int fortune = new java.util.Random().nextInt(3);
-	  fortune++;
-	  System.out.println("占いの結果が出ました！");
-	  System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
-	  System.out.println("1:大吉 2:中吉 3:小吉 4:凶");
+    for (int i = 1; i < 10; i++) {
+     for (int j = 1;j < 10;j++) {
+       System.out.print(i * j);  // 掛け算の結果を出力
+       System.out.print(" ");    // 空白を出力
+     }
+     System.out.println("");     // 改行を出力
+    }
   }
 }
