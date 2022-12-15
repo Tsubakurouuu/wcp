78c557985f41072fe856995599962517dae94431
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-15 02:24:48 +0000

    スッキリわかるJava入門4.9 4-1

--- Main.java
+++ Main.java
@@ -1,12 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    int[][] scores = new int[2][3];
-    scores[0][0] = 40;
-    scores[0][1] = 50;
-    scores[0][2] = 60;
-    scores[1][0] = 80;
-    scores[1][1] = 60;
-    scores[1][2] = 70;
-    System.out.println(scores[1][1]);
+    int[] points = new int[4];
+    double[] weigths = new double[5];
+    boolean[] answers = new boolean[3];
+    String[] names = new String[3];
   }
 }
