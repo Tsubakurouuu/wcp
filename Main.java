cdc544ed587e407ab63ec2cb68ca91738bce652b
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-15 22:40:42 +0000

    スッキリわかるJava入門5.8 5-1

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 public class Main {
-  // int型配列を受け取り、
-  // 配列内の要素すべてに1を加えるメソッド
-  public static void incArray(int[] array) {
-    for (int i = 0; i < array.length; i++) {
-      array[i]++;
-    }
-  }
   public static void main(String[] args) {
-    int[] array = {1, 2, 3};
-    incArray(array);
-    for (int i : array) {
-      System.out.println(i);
-    }
+      introduceOnself();
+  }
+
+  public static void introduceOnself () {
+	  String name = "Yuya";
+	  System.out.println(name);
+	  int age = 24;
+	  System.out.println(age);
+	  double height = 177.2;
+	  System.out.println(height);
+	  char zodiac = '虎';
+	  System.out.println(zodiac);
   }
 }
