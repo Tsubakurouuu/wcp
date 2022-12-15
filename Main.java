9e88173c79fe859f27809c2d2ca86dbc9fbbcefe
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-15 02:41:04 +0000

    スッキリわかるJava入門4.9 4-4

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 public class Main {
   public static void main(String[] args) {
-    int[] moneyList = { 121902, 8302, 55100};
-    for (int i = 0; i <  moneyList.length; i++) {
-    	System.out.println(moneyList[i]);
-    }
-    for (int a : moneyList) {
-    	System.out.println(a);
-    }
+	  int[] numbers = { 3, 4, 9 };
+	  System.out.println("1桁の数字を入力してください");
+	  int input = new java.util.Scanner(System.in).nextInt();
+	  for (int i =0; i < numbers.length; i++) {
+		  if (input == numbers[i]) {
+			  System.out.println("アタリ！");
+			  break;
+		  }
+	  }
   }
 }
