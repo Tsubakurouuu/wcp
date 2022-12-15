6c3fc10e0cc1184c3cebde2238802ee5c9dd775e
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-15 02:05:29 +0000

    スッキリわかるJava入門4.7 4-18

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 public class Main {
   public static void main(String[] args) {
-    int[] scores = {20, 30, 40, 50, 80};
-    int count = 0;
-    for (int i = 0; i < scores.length; i++) {
-      if (scores[i] >= 50) {
-        count++;
-      }
-    }
-    System.out.println("50点以上の科目の数は:" + count);
+    int[][] scores = new int[2][3];
+    scores[0][0] = 40;
+    scores[0][1] = 50;
+    scores[0][2] = 60;
+    scores[1][0] = 80;
+    scores[1][1] = 60;
+    scores[1][2] = 70;
+    System.out.println(scores[1][1]);
   }
 }
