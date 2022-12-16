61d1a9e8cca9f91ec5324459ccdbbb0dec45a5ad
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-16 01:12:43 +0000

    スッキリわかるJava入門6.8 6-1

--- Main.java
+++ Main.java
@@ -1,18 +1,10 @@
+import comment.Zenhan;
+
 public class Main {
-  	public static void main(String[] args) {
-      	double triangle = calcTriangleArea(5.0,10.0);
-      	double circle = calcCircleArea(5.0);
-      	System.out.println(triangle);
-      	System.out.println(circle);
-  	}
-
-  	public static double calcTriangleArea(double bottom, double height) {
-  		double triangle = bottom * height / 2;
-  		return triangle;
-  	}
-
-  	public static double calcCircleArea(double radius) {
-	  	double circle = radius * radius * 3.14;
-	  	return circle;
-  	}
+  public static void main(String[] args) throws Exception {
+    Zenhan.doWarusa();
+    Zenhan.doTogame();
+    comment.Kouhan.callDeae();
+    comment.Kouhan.showMondokoro();
+  }
 }
