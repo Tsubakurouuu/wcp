5940972487fb1704d12d53d94d6860bf02d0b93a
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-16 07:32:56 +0000

    スッキリわかるJava入門8-6 8-4

--- Cleric.java
+++ Cleric.java
@@ -12,4 +12,13 @@ public class Cleric {
 		this.hp = this.MAX_HP;
 		System.out.println(this.name + "のHPは最大まで回復した");
 	}
+
+	public int pray(int sec) {
+	    System.out.println(this.name + "は" + sec + "秒間祈った！");
+	    int recover = new java.util.Random().nextInt(3) + sec;
+	    int recoverActual = Math.min(this.MAX_MP - this.mp, recover);
+	    this.mp += recoverActual;
+	    System.out.println("MPが" + recoverActual + "回復した");
+	    return recoverActual;
+	}
 }
