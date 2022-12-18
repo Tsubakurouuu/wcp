263f13165b85e0cec0e95f77c06e1c6448505dac
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-18 06:17:06 +0000

    J-4

--- Main.java
+++ Main.java
@@ -1,10 +1,25 @@
 
 public class Main {
   	public static void main(String[] args) {
-  		String str = "";
-  		for (int i = 1; i <= 10; i++) {
-  			str = str + i + ",";
+  		int[] a = new int[20];
+  		for (int i = 0; i < a.length; i++) {
+  			a[i] = (i + 1) * 5;
   		}
-  		System.out.println(str.substring(0, str.length() -1));
+
+  		String kisuu = "";
+  		for (int i = 0; i <= 10; i++) {
+  			if (a[i] % 2 != 0) {
+  				kisuu = kisuu + a[i] + ",";
+  			}
+  		}
+  		System.out.println("奇数：" + kisuu.substring(0, kisuu.length() -1));
+
+  		String guusuu = "";
+  		for (int i = 11; i <= 19; i++) {
+  			if (a[i] % 2 == 0) {
+  				guusuu = guusuu + a[i] + ",";
+  			}
+  		}
+  		System.out.println("奇数：" + guusuu.substring(0, guusuu.length() -1));
   	}
 }
