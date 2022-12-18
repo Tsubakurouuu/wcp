a83287c431804adc59392a6c5503e69e8e65144d
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-18 05:58:57 +0000

    J-3

--- Main.java
+++ Main.java
@@ -1,15 +1,10 @@
 
 public class Main {
-  public static void main(String[] args) {
-	int c = 0;
-	int s = 0;
-    for (int i = 1; i <= 10; i++) {
-    	if (i % 2 == 0) {
-    		c += 1;
-    		s += i;
-    	}
-    }
-    System.out.println("偶数の数は" + c);
-    System.out.println("偶数の合計は" + s);
-  }
+  	public static void main(String[] args) {
+  		String str = "";
+  		for (int i = 1; i <= 10; i++) {
+  			str = str + i + ",";
+  		}
+  		System.out.println(str.substring(0, str.length() -1));
+  	}
 }
