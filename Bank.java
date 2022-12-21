e5e3b2054572b2a95433c702d11ce7c56d2934e0
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-21 02:59:29 +0000

    スッキリわかるJava入門14-5 14-1

---
+++ Bank.java
@@ -0,0 +1,25 @@
+
+public class Bank {
+	String accountNumber;
+	int balance;
+
+	public String toString() {
+		return "¥" + balance + "(口座番号：" + accountNumber + ")";
+	}
+
+	public boolean equals(Object o) {
+		if(this == o) {
+			return true;
+		}
+		if(o instanceof Bank) {
+			Bank a = (Bank)o;
+			String an1 = this.accountNumber.trim();
+			String an2 = a.accountNumber.trim();
+			if(an1.equals(an2)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
