f28f43827fee785ae11da266adb5a73abc7f2b50
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-21 09:03:27 +0000

    スッキリわかるJava入門15-9 15-5

--- Main.java
+++ Main.java
@@ -1,17 +1,14 @@
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.text.SimpleDateFormat;
 
 public class Main {
   	public static void main(String[] args) {
-    	Date now = new Date();
-    	Calendar c = Calendar.getInstance();
-    	c.setTime(now);
-    	int day = c.get(Calendar.DAY_OF_MONTH);
-    	day += 100;
-    	c.set(Calendar.DAY_OF_MONTH, day);
-    	Date future = c.getTime();
-    	SimpleDateFormat f = new SimpleDateFormat("西暦yyyy年MM月dd日");
-    	System.out.println(f.format(future));
+
+    	LocalDate now = LocalDate.now();
+    	LocalDate future = now.plusDays(100);
+    	DateTimeFormatter fmt = DateTimeFormatter.ofPattern("西暦yyyy年MM月dd日");
+    	System.out.println(future.format(fmt));
   	}
 }
