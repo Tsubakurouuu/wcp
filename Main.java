1c0cfb61201d60a27117a604ef8fa6f557ad9b96
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-21 08:04:31 +0000

    スッキリわかるJava入門15-7 15-16

--- Main.java
+++ Main.java
@@ -1,16 +1,21 @@
 import java.time.*;
+import java.time.format.*;
 
 public class Main {
   public static void main(String[] args) {
-    // LocalDateTimeの生成方法
-    LocalDateTime l1 = LocalDateTime.now();
-    LocalDateTime l2 = LocalDateTime.of(2020, 1, 1, 9, 5, 0, 0);
+    // 文字列からLocalDateを生成
+    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+    LocalDate ldate = LocalDate.parse("2020/09/22", fmt);
 
-    // LocalDateTimeとZoneDateTimeの相互変換
-    ZonedDateTime z1 = l2.atZone(ZoneId.of("Europe/London"));
-    LocalDateTime l3 = z1.toLocalDateTime();
+    // 1000日後を計算する
+    LocalDate ldatep = ldate.plusDays(1000);
+    String str = ldatep.format(fmt);
+    System.out.println("1000日後は" + str);
 
-    System.out.println(z1);
-    System.out.println(l3);
+    // 現在日付との比較
+    LocalDate now = LocalDate.now();
+    if(now.isAfter(ldatep)){
+      System.out.println("1000日後は過去日付です");
+    }
   }
 }
