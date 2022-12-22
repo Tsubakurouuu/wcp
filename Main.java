ff307d438fc43a74b793e76e6e7e5a26c1b73827
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-22 02:52:18 +0000

    スッキリわかるJava入門17-9 17-2

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
 import java.util.ArrayList;
-import java.util.*;
+import java.io.*;
 
 public class Main {
   	public static void main(String[] args) {
-  		String s = null;
-  		System.out.println(s.length());
+  		try {
+  			String s = null;
+  			System.out.println(s.length());
+  		} catch(NullPointerException e) {
+  			System.out.println("NullPointException例外をcatchしました。");
+  			System.out.println("--スタックトレース(ここから)--");
+  			e.printStackTrace();
+  			System.out.println("--スタックトレース(ここまで)--");
+  		}
   	}
 }
