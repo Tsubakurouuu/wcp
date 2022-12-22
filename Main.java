48de6c570338c40a42e82da17bca644373741b5b
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-22 00:37:12 +0000

    スッキリわかるJava入門16-8 16-2

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-  public static void main(String[] args) {
-    Map<String, Integer> prefs = new HashMap<String, Integer>();
-    prefs.put("京都府", 255);
-    prefs.put("東京都", 1261);
-    prefs.put("熊本県", 181);
-    int tokyo = prefs.get("東京都");
-    System.out.println("東京都の人口は、" + tokyo);
-    prefs.remove("京都府");
-    prefs.put("熊本県", 182);
-    int kumamoto = prefs.get("熊本県");
-    System.out.println("熊本県の人口は、" + kumamoto);
-  }
+  	public static void main(String[] args) {
+  		Hero h1 = new Hero("斉藤");
+  		Hero h2 = new Hero("鈴木");
+  		List<Hero> heroes = new ArrayList<Hero>();
+  		heroes.add(h1);
+  		heroes.add(h2);
+  		for(Hero h : heroes) {
+  			System.out.println(h.getName());
+  		}
+
+  	}
 }
