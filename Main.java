c4c83084bb606807b475617d8a3405e5a6111dbf
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-22 03:00:47 +0000

    スッキリわかるJava入門17-9 17-3

--- Main.java
+++ Main.java
@@ -4,13 +4,9 @@ import java.io.*;
 public class Main {
   	public static void main(String[] args) {
   		try {
-  			String s = null;
-  			System.out.println(s.length());
-  		} catch(NullPointerException e) {
-  			System.out.println("NullPointException例外をcatchしました。");
-  			System.out.println("--スタックトレース(ここから)--");
-  			e.printStackTrace();
-  			System.out.println("--スタックトレース(ここまで)--");
+  			int i = Integer.parseInt("三");
+  		} catch (NumberFormatException e) {
+  			System.out.println("NumberFormatException例外をcatchしました");
   		}
   	}
 }
