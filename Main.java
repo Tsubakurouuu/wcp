35c31930ee5a801c7972e7cf61e596d5009209e4
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-22 02:40:56 +0000

    スッキリわかるJava入門17-9 17-1

--- Main.java
+++ Main.java
@@ -3,15 +3,7 @@ import java.util.*;
 
 public class Main {
   	public static void main(String[] args) {
-  		Hero h1 = new Hero("斉藤");
-  		Hero h2 = new Hero("鈴木");
-  		Map<Hero, Integer> heroes = new HashMap<Hero, Integer>();
-  		heroes.put(h1, 3);
-  		heroes.put(h2, 7);
-  		for(Hero h : heroes.keySet()) {
-  			int i = heroes.get(h);
-  			System.out.println( h.getName() + "が倒した数=" + i);
-  		}
-
+  		String s = null;
+  		System.out.println(s.length());
   	}
 }
