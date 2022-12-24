3c38cc9f7a1f7cb4cc00cfb432626d783cf7bc11
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-24 00:41:08 +0000

    Java_SE_Bronze問題集 6-22

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 
 public class Main {
   	public static void main(String[] args) {
-  		int i = 0;
-  		while(i < 5) {
-  			i++;
-  		}
-  		System.out.println(i);
-
+  		Part p1 = new Part();
+  		Part p2 = new Part();
+  		p1.count = 1;
+  		System.out.println(p2.count);
   	}
+}
+
+class Part {
+	public static int count = 0;
 }
