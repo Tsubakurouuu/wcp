fdf1a7195d095fdbfd7098b084e5a3d7e4a7c78b
Tsubakurouuu <yzpolice2896@icloud.com>
2022-12-25 23:17:17 +0000

    Java_SE_Bronze問題集 7-5

--- Main.java
+++ Main.java
@@ -1,22 +1,8 @@
 
 public class Main {
   	public static void main(String[] args) {
-  		Counter c1 = new Counter();
-  		Counter c2 = new Counter();
-  		c1.pluss();
-  		c2.pluss();
-  		System.out.println(Counter.getCount());
+  		B b = new B();
   	}
 }
 
-class Counter {
-	static int count = 0;
 
-	public static int getCount() {
-		return count;
-	}
-
-	public static void pluss() {
-		count++;
-	}
-}
