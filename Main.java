11dd944b34eed6456c317b3e52ba890319c0b8eb
Tsubakurouuu <yzpolice2896@icloud.com>
2023-01-01 22:18:48 +0000

    Java_SE_Bronze問題集 9-3

--- Main.java
+++ Main.java
@@ -1,21 +1,13 @@
 
 public class Main {
 	public static void main(String[] args) {
-		String a1 = "A";
-		String a2 = new String("A");
-		String a3 = new String("A");
-
-		if(a1 == a2) {
-			System.out.println("a1 == a2");
-		} else {
-			System.out.println("a1 != a2");
-		}
-
-		if(a2 == a3) {
-			System.out.println("a2 == a3");
-		} else {
-			System.out.println("a2 != a3");
-		}
+		int a = 3 * 5;
+		int b = 25 - 10;
+		if(a > b) System.out.println("A");
+		if(a < b) System.out.println("B");
+		if(a == b) System.out.println("C");
+		else System.out.println("D");
+		
 	}
 }
 
