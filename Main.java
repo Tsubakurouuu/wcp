565876aacaa67eeeb845d678073b17ede6435584
Tsubakurouuu <yzpolice2896@icloud.com>
2023-01-02 05:43:49 +0000

    Java_SE_Bronze問題集 9-5

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
 
 public class Main {
 	public static void main(String[] args) {
-		int a = 3 * 5;
-		int b = 25 - 10;
-		if(a > b) System.out.println("A");
-		if(a < b) System.out.println("B");
-		if(a == b) System.out.println("C");
-		else System.out.println("D");
-		
+		int a = 0;
+		do {
+			a++;
+			System.out.print("hi ");
+		} while(a < 4);
 	}
 }
 
