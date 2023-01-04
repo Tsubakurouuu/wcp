cbc40131280f85c5b311036e7d72843aa2fb2de9
Tsubakurouuu <yzpolice2896@icloud.com>
2023-01-04 00:04:00 +0000

    Java SE Silver 2-4 変数の初期化

--- Main.java
+++ Main.java
@@ -1,14 +1,10 @@
 
 public class Main {
 	public static void main(String[] args) {
-		int result = 0;
-		long start = System.currentTimeMillis();
-
-		for(int i = 0;i < 100000000; i++) {
-			result += i;
-		}
-		long end = System.currentTimeMillis();
-		System.out.println((end -start) + "ms");
+		int num = 10;
+		long num = 30;
+		//long num3 = 10000000000;
+		//float num4 = 10.0;
 	}
 }
 
