464126bf49713372584e974124f9cca162f1800e
Tsubakurouuu <yzpolice2896@icloud.com>
2023-01-05 00:06:04 +0000

    Java SE Silver 3-12

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
 public class Main {
   public static void main(String[] args) {
-    String s1 = "Hello";
-    String s2 = new String("Hello");
-    String s3 = "Hello";
-    String s4 = s2.intern();
+    String s1 = "X";
+    String s2 = s1 + "Y";
     System.out.println("s1 == s2   : " + ( s1 == s2 ));
-    System.out.println("s1 == s3   : " + ( s1 == s3 ));
-    System.out.println("s1 == s4   : " + ( s1 == s4 ));
-    System.out.println("s2 == s4   : " + ( s2 == s4 ));
+
+    String s3 = "X";
+    String s4 = s3.concat("Y");
+    System.out.println("s3 == s4   : " + ( s3 == s4 ));
+
+    StringBuilder s5 = new StringBuilder ("X");
+    StringBuilder s6 = s5.append("Y");
+    System.out.println("s5 == s6   : " + ( s5 == s6 ));
   }
 }
