005eb710e2007befdab0f6e4cc71be42cdf63a7e
Tsubakurouuu <yzpolice2896@icloud.com>
2023-01-05 03:18:32 +0000

    Java SE Silver 4-11

--- Main.java
+++ Main.java
@@ -1,8 +1,25 @@
 public class Main {
   public static void main(String[] args) {
-    int num = 20;
-    String str = "numの値は";
-    str += num < 10 ? "10未満" : "10以上";
-    System.out.println(str);
+    loop1:     // ���x��loop1���w��
+    for(int x = 0; x < 3 ; x++){
+      for(int y = 0; y < 3; y++){
+        System.out.println("x = " + x + " y = " + y);
+        if(x == 1 && y ==1){
+          System.out.println("break文の実行");
+          break loop1;  // ���x��loop1��break
+        }
+      }
+    }
+    System.out.println("------------------------------");
+    loop2:     // ���x��loop2���w��
+    for(int x = 0; x < 3 ; x++){
+      for(int y = 0; y < 3; y++){
+        System.out.println("x = " + x + " y = " + y);
+        if(x == 1 && y ==1){
+          System.out.println("continue文の実行");
+          continue loop2;  // ���x��loop2��continue
+        }
+      }
+    }
   }
 }
