13a76560ff1b619ba46b4282dfc927c9304aeedb
Tsubakurouuu <yzpolice2896@icloud.com>
2023-01-08 14:52:33 +0000

    Java SE Silver 7-4

--- Main.java
+++ Main.java
@@ -1,13 +1,18 @@
-public class Main {
-  public static void main(String[] args) {
+class Test {
+  void loop() throws ArrayIndexOutOfBoundsException {
     int[] num = {10, 20, 30};
     for(int i = 0; i < 4; i++){
-      try {
-        System.out.print("num :" + num[i]);
-        System.out.println(" : " + (i+1) + "回目のループ");
-      } catch(ArrayIndexOutOfBoundsException e) {
-        System.out.println("例外が発生しました");
-      }
+      System.out.println("num :" + num[i]);
+    }
+  }
+}
+public class Main {
+  public static void main(String[] args) {
+    try {
+      Test t = new Test();
+      t.loop();
+    } catch(ArrayIndexOutOfBoundsException e) {
+      System.out.println("例外が発生しました");
     }
     System.out.println("-- end --");
   }
