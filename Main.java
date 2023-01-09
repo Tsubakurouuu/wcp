dbc28b352c304dc192f0c94fdf6a4bc096959a1c
Tsubakurouuu <yzpolice2896@icloud.com>
2023-01-09 00:04:47 +0000

    Java SE Silver 7-3

--- Main.java
+++ Main.java
@@ -1,25 +1,16 @@
-class MyException extends Exception { // �Ǝ���O�N���X
-  private int age;
-  public void setAge(int age) { this.age = age; }
-  public int getAge() { return this.age; }
-}
-
 public class Main {
   public static void main(String[] args) {
-    try {
-      int age = -10;
-      checkAge(age);
-    } catch (MyException e) {
-      System.out.println("不正な値です。 : " + e.getAge());
+    int[] num = {10, 20, 30};
+    for(int i = 0; i < 4; i++){
+      try {
+        System.out.print("num :" + num[i]);
+        System.out.println(" : " + (i+1) + "回目のループ");
+      } catch(ArrayIndexOutOfBoundsException e) {
+        System.out.println("例外が発生しました");
+      } finally {
+        System.out.println("-- finallyの実行");
+      }
     }
-  }
-  public static void checkAge(int age) throws MyException {
-    if(age >= 0){
-      System.out.println("OK");
-    }else{
-      MyException e = new MyException();
-      e.setAge(age);
-      throw e;
-    }
+    System.out.println("-- end --");
   }
 }
