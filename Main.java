8a81fe4de99be1030faf0b76c612effc362a1366
Tsubakurouuu <yzpolice2896@icloud.com>
2023-01-11 10:26:17 +0000

    Java SE Silver 問題6-18

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
+class Animal {
+  void x() { System.out.print("Animal "); }
+  void y() { x(); }
+}
+class Cow extends Animal {
+  void x() { System.out.print("Cow "); }
+  void z() { x(); }
+}
 public class Main {
   public static void main(String[] args) {
-    int[] num = {10, 20, 30};
-    for(int i = 0; i < 4; i++){
-      try {
-        System.out.print("num :" + num[i]);
-        System.out.println(" : " + (i+1) + "回目のループ");
-      } catch(ArrayIndexOutOfBoundsException e) {
-        System.out.println("例外が発生しました");
-      } finally {
-        System.out.println("-- finallyの実行");
-      }
-    }
-    System.out.println("-- end --");
+    Cow obj = new Cow();
+    obj.y();
+    obj.z();
   }
 }
