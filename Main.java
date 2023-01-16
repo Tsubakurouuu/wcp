2e044857fd0605dd478fcdeb6d1bee58ea53fb34
Tsubakurouuu <yzpolice2896@icloud.com>
2023-01-16 00:13:56 +0000

    Java SE 11 Silver 9-12

--- Main.java
+++ Main.java
@@ -1,18 +1,17 @@
+import java.util.ArrayList;
+
 public class Main {
-  public static void main(String[] args) {
-    A a = new B();
-  }
-
-}
-
-class A {
-	  public A() {
-		  System.out.println("A");
-	  }
-}
-
-class B extends A {
-	  public B() {
-		  System.out.println("B");
-	  }
+  	public static void main(String[] args) {
+  		ArrayList<String> list = new ArrayList<>();
+  		list.add("A");
+  		list.add("B");
+  		list.add("C");
+  		for(String str : list) {
+  			if("B".equals(str)) {
+  				list.remove(str);
+  			} else {
+  				System.out.println(str);
+  			}
+  		}
+  	}
 }
