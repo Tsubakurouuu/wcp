1b57488d094ddb74baacaa4684c615066e6d2516
Tsubakurouuu <yzpolice2896@icloud.com>
2023-01-19 02:10:37 +0000

    Java SE 11 Silver 11-80

--- Main.java
+++ Main.java
@@ -1,18 +1,25 @@
 
 public class Main {
   	public static void main(String[] args) {
-  		System.out.println(test(null));
+  		String[][] array = new String[2][];
+  		array[0] = new String[2];
+  		array[1] = new String[5];
+  		int i = 97;
+  		for(int a = 0; a < array.length; a++) {
+  			for(int b =0; b < array.length; b++) {
+  				array[a][b] = "" + i;
+  				i++;
+  			}
+  		}
+
+  		for(String[] tmp : array) {
+  			for(String s : tmp) {
+  				System.out.print(s + " ");
+  			}
+  			System.out.println();
+  		}
   	}
 
-  	private static String test(Object obj) {
-  		try {
-  			System.out.println(obj.toString());
-  		} catch (NullPointerException e) {
-  			return "A";
-  		} finally {
-  			System.out.println("B");
-  		}
-  		return "C";
-  	}
+
 
 }
