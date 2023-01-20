bb80f6a09632c731c930d4076c9e07f442cd8cfd
Tsubakurouuu <yzpolice2896@icloud.com>
2023-01-20 01:57:32 +0000

    Java SE 11 mogi1-24

--- Main.java
+++ Main.java
@@ -1,25 +1,7 @@
-
 public class Main {
-  	public static void main(String[] args) {
-  		String[][] array = new String[2][];
-  		array[0] = new String[2];
-  		array[1] = new String[5];
-  		int i = 97;
-  		for(int a = 0; a < array.length; a++) {
-  			for(int b =0; b < array.length; b++) {
-  				array[a][b] = "" + i;
-  				i++;
-  			}
-  		}
-
-  		for(String[] tmp : array) {
-  			for(String s : tmp) {
-  				System.out.print(s + " ");
-  			}
-  			System.out.println();
-  		}
-  	}
-
-
-
+  public static void main(String[] args) {
+    int a = 100;  int b = ++a;
+    int c = b++;  int d = ++c;
+    System.out.println((a < b) ? (b < c) : (c < d) ? b : c);
+  }
 }
