605683d78869d4f2e4df0266d70c67baa4b2c613
Tsubakurouuu <yzpolice2896@icloud.com>
2023-01-21 01:26:10 +0000

    Java SE 11 mogi2-8

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
   public static void main(String[] args) {
-    int a = 100;  int b = ++a;
-    int c = b++;  int d = ++c;
-    System.out.println((a < b) ? (b < c) : (c < d) ? b : c);
+    String[] ary = {"A", null, "C"};
+    int count = 0;
+    for(String str : ary){
+      count += ary.length;
+    }
+    System.out.println(count);
   }
 }
